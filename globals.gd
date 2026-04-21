de10bb08f9cc738d97edf657ac20bb012b19f704
Matt Giuca <matt.giuca@gmail.com>
2026-04-21 14:21:43 +1000

    Host and join.

---
+++ globals.gd
@@ -0,0 +1,5 @@
+class_name Globals
+extends Object
+
+const PORT : int = 28744
+const MAX_CLIENTS : int = 1
--- menu.gd
+++ menu.gd
@@ -3,15 +3,38 @@ extends Control
 @onready var lbl_my_ip : Label = %LblMyIP
 @onready var txt_join_ip : LineEdit = %TxtJoinIP
 
+@onready var accept_dialog : AcceptDialog = $AcceptDialog
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
   # TODO: Get my IP address in lbl_my_ip.
   pass
 
 func _on_btn_host_pressed() -> void:
+  print('Hosting game')
+  var peer := ENetMultiplayerPeer.new()
+  var e := peer.create_server(Globals.PORT, Globals.MAX_CLIENTS)
+  if e != Error.OK:
+    show_dialog('Connection error', 'Could not connect to server')
+    return
+
+  multiplayer.multiplayer_peer = peer
   get_tree().change_scene_to_file('res://level.tscn')
 
 func _on_btn_join_pressed() -> void:
   var ip := txt_join_ip.text
   print('Attempting to connect to %s' % ip)
-  # TODO
+
+  var peer := ENetMultiplayerPeer.new()
+  var e := peer.create_client(ip, Globals.PORT)
+  if e != Error.OK:
+    show_dialog('Connection error', 'Could not connect to server')
+    return
+
+  multiplayer.multiplayer_peer = peer
+  get_tree().change_scene_to_file('res://level.tscn')
+
+func show_dialog(title: String, message: String) -> void:
+  accept_dialog.title = title
+  accept_dialog.dialog_text = message
+  accept_dialog.show()
