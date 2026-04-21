4f7ac2acc80af9651c5a249dacb1d04df14195cf
Matt Giuca <matt.giuca@gmail.com>
2026-04-21 14:39:01 +1000

    Abstract to NetManager.

--- menu.gd
+++ menu.gd
@@ -7,30 +7,23 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  lbl_my_ip.text = Globals.get_my_ip()
+  lbl_my_ip.text = NetManager.get_my_ip()
 
 func _on_btn_host_pressed() -> void:
-  print('Hosting game')
-  var peer := ENetMultiplayerPeer.new()
-  var e := peer.create_server(Globals.PORT, Globals.MAX_CLIENTS)
+  var e := NetManager.start_host_game()
   if e != Error.OK:
     show_dialog('Connection error', 'Could not connect to server')
     return
 
-  multiplayer.multiplayer_peer = peer
   get_tree().change_scene_to_file('res://level.tscn')
 
 func _on_btn_join_pressed() -> void:
   var ip := txt_join_ip.text
-  print('Attempting to connect to %s' % ip)
-
-  var peer := ENetMultiplayerPeer.new()
-  var e := peer.create_client(ip, Globals.PORT)
+  var e := NetManager.start_join_game(ip)
   if e != Error.OK:
     show_dialog('Connection error', 'Could not connect to server')
     return
 
-  multiplayer.multiplayer_peer = peer
   get_tree().change_scene_to_file('res://level.tscn')
 
 func show_dialog(title: String, message: String) -> void:
--- net_manager.gd
+++ net_manager.gd
@@ -1,10 +1,30 @@
-class_name Globals
-extends Object
+extends Node
 
 const PORT : int = 28744
 const MAX_CLIENTS : int = 1
 
-static func get_my_ip() -> String:
+func start_host_game() -> Error:
+  print('Hosting game')
+  var peer := ENetMultiplayerPeer.new()
+  var e := peer.create_server(PORT, MAX_CLIENTS)
+  if e != Error.OK:
+    return e
+
+  multiplayer.multiplayer_peer = peer
+  return Error.OK
+
+func start_join_game(ip: String) -> Error:
+  print('Attempting to connect to %s' % ip)
+
+  var peer := ENetMultiplayerPeer.new()
+  var e := peer.create_client(ip, PORT)
+  if e != Error.OK:
+    return e
+
+  multiplayer.multiplayer_peer = peer
+  return Error.OK
+
+func get_my_ip() -> String:
   var ipv4_re := RegEx.create_from_string(r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$')
   for interface in IP.get_local_interfaces():
     var interface_name : String = interface['name']
