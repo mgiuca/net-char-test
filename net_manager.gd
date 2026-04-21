45a23f89603f2d93f670ca8aeb281257aa508a47
Matt Giuca <matt.giuca@gmail.com>
2026-04-21 14:43:17 +1000

    Log net signals.

--- net_manager.gd
+++ net_manager.gd
@@ -3,6 +3,13 @@ extends Node
 const PORT : int = 28744
 const MAX_CLIENTS : int = 1
 
+func _ready() -> void:
+  multiplayer.peer_connected.connect(_on_peer_connected)
+  multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+  multiplayer.connected_to_server.connect(_on_connected_to_server)
+  multiplayer.connection_failed.connect(_on_connection_failed)
+  multiplayer.server_disconnected.connect(_on_server_disconnected)
+
 func start_host_game() -> Error:
   print('Hosting game')
   var peer := ENetMultiplayerPeer.new()
@@ -43,3 +50,20 @@ func get_my_ip() -> String:
       return addr
 
   return ''
+
+func _on_peer_connected(id: int) -> void:
+  print('Peer connected: %d' % id)
+
+func _on_peer_disconnected(id: int) -> void:
+  print('Peer disconnected: %d' % id)
+
+# Client signals
+
+func _on_connected_to_server() -> void:
+  print('Connected to server')
+
+func _on_connection_failed() -> void:
+  print('Connection failed')
+
+func _on_server_disconnected() -> void:
+  print('Server disconnected')
