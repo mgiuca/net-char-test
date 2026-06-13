6d97e0f346282105726255fc9cef032fe70ce7d5
Matt Giuca <matt.giuca@gmail.com>
2026-06-13 18:02:07 +1000

    NetManager: Better implementation of get_my_ip.
    Works properly on Windows and simpler.

--- net_manager.gd
+++ net_manager.gd
@@ -32,22 +32,15 @@ func start_join_game(ip: String) -> Error:
   return Error.OK
 
 func get_my_ip() -> String:
-  var ipv4_re := RegEx.create_from_string(r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$')
-  for interface in IP.get_local_interfaces():
-    var interface_name : String = interface['name']
-    # Not a "virtual" IP address (on Linux).
-    if interface_name.begins_with('virbr'):
-      continue
-    for addr : String in interface['addresses']:
-      # Not IPv6.
-      if not ipv4_re.search(addr):
-        continue
-      # Not localhost.
-      if addr == '127.0.0.1':
-        continue
+  var addresses := IP.get_local_addresses()
 
-      # Return the first address that meets the above criteria.
-      return addr
+  for ip in addresses:
+    # Filter out:
+    # - IPv6 addresses (with ':')
+    # - Loopback ('127.x.x.x')
+    # - Invalid / disconnected ('169.254.x.x')
+    if ':' not in ip and not ip.begins_with('127.') and not ip.begins_with('169.254.'):
+      return ip
 
   return ''
 
